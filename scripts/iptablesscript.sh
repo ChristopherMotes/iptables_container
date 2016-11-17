49560cf5bfc4cc881aaadcb08e85ed01b5b24121
EC2 Default User <ec2-user@ip-172-31-19-196.ec2.internal>
2016-11-17 13:49:56 +0000

    tightened iptables rules

--- scripts/iptablesscript.sh
+++ scripts/iptablesscript.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
 iptables -X
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -A -p tcp -s 172.18.0.23 -j ACCEPT
+iptables -A OUTPUT -A -p tcp -d 172.18.0.23 -j ACCEPT
+iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
 iptables -t nat -A PREROUTING -p tcp --dport 5000 -j DNAT --to-destination 172.18.0.23:5000
 iptables -t nat -A POSTROUTING -p tcp -d 172.18.0.23 --dport 5000 -j SNAT --to-source 172.18.0.33
 tcpdump port 5000
